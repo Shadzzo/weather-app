9d8e884080ec9e792b8c0108104ca653bd26e3e4
Shadzzo <97727190+Shadzzo@users.noreply.github.com>
2023-03-31 16:53:16 +0300

    Initial commit

---
+++ pages/CityView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ClientOnly>
+            <Suspense>
+                <AsyncCityView />
+                <template #fallback>
+                    <Loading />
+                </template>
+            </Suspense>
+        </ClientOnly>  
+    </div>
+</template>
+
+<script setup>
+import AsyncCityView from '~~/components/AsyncCityView.vue'
+
+</script>
+
+<style>
+</style>
